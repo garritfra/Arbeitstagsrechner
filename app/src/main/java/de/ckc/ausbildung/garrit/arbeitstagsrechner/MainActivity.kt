0c832e31d3a53ae485c250c2a17e04296cd20067
Franke, Garrit <Garrit.Franke@ckc.de>
2018-04-27 11:15:42 +0200

    Initial commit

---
+++ app/src/main/java/de/ckc/ausbildung/garrit/arbeitstagsrechner/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.ckc.ausbildung.garrit.arbeitstagsrechner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
