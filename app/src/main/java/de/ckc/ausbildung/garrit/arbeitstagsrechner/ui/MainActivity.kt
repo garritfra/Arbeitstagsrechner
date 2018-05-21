e8577d571e5fb319df9f3e04e9331a7589149190
garritfra <garritfranke@gmail.com>
2018-05-21 19:32:51 +0200

    add saveDateIntervalActivity

--- app/src/main/java/de/ckc/ausbildung/garrit/arbeitstagsrechner/ui/MainActivity.kt
+++ app/src/main/java/de/ckc/ausbildung/garrit/arbeitstagsrechner/ui/MainActivity.kt
@@ -1,10 +1,14 @@
-package de.ckc.ausbildung.garrit.arbeitstagsrechner
+package de.ckc.ausbildung.garrit.arbeitstagsrechner.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import de.ckc.ausbildung.garrit.arbeitstagsrechner.R
 import de.ckc.ausbildung.garrit.arbeitstagsrechner.Utils.DateParser
 import de.ckc.ausbildung.garrit.arbeitstagsrechner.model.DateInterval
 import java.text.ParseException
@@ -19,14 +23,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val calculateIntervalButton = findViewById<Button>(R.id.calculate_interval_button)
+        val floatingActionButton = findViewById<FloatingActionButton>(R.id.floating_action_button)
+
+        floatingActionButton.setOnClickListener((View.OnClickListener {
+            handleFloatingActionButtonClicked()
+        }))
 
         calculateIntervalButton.setOnClickListener({
-            handleCalculateDateButtonClicked()
+            handleCalculateIntervalButtonClicked()
         })
 
     }
 
-    private fun handleCalculateDateButtonClicked() {
+    private fun handleFloatingActionButtonClicked() {
+        val saveDateIntervalActivityIntent = Intent(this, SaveDateIntervalActivity::class
+                .java)
+        startActivity(saveDateIntervalActivityIntent)
+    }
+
+    private fun handleCalculateIntervalButtonClicked() {
 
         val dateStartEditText = findViewById<EditText>(R.id.date_start_editText)
         val dateEndEditText = findViewById<EditText>(R.id.date_end_editText)
---
+++ app/src/main/java/de/ckc/ausbildung/garrit/arbeitstagsrechner/ui/SaveDateIntervalActivity.kt
@@ -0,0 +1,13 @@
+package de.ckc.ausbildung.garrit.arbeitstagsrechner.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import de.ckc.ausbildung.garrit.arbeitstagsrechner.R
+
+class SaveDateIntervalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_date_interval)
+    }
+}
